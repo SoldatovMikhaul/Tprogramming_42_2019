fb720ee64680dce596781569dd9f0adde8e332d0
SoldatovMikhaul <55295303+SoldatovMikhaul@users.noreply.github.com>
2019-11-02 13:21:30 +0300

    Update Program.cs

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,13 +1,28 @@
-﻿using System;
+using System;
 
-namespace CourseApp
+namespace lab1matfunc
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
-    }
+class Program
+{
+static double Func(double a, double b, double x)
+{
+double y = (Math.Pow(a, x) - Math.Pow(b, x)) * Math.Pow(a * b, 1.0 / 3) / Math.Log10(a / b);
+return y;
+}
+static void Main(string[] args)
+{
+double a = 0.4;
+double b = 0.8;
+double y;
+for (double x = 3.2; x < 6.3; x = x + 0.6)
+{
+Console.WriteLine(Func(a, b, x));
+}
+for (int i = 0; i < 5; i++)
+{
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Func(a, b, x));
+}
+}
+}
 }
