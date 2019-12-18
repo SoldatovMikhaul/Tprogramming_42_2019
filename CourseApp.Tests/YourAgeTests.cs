b68a450885f4806904eb17a380d7dc2a88057c31
misha.soldatov.96@mail.ru <misha.soldatov.96@mail.ru>
2019-12-18 21:19:54 +0300

    часы без тестов

---
+++ CourseApp.Tests/YourAgeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace CourseApp.Tests
+{
+    public class YourAgeTests
+    {
+        /*[Theory]
+        [InlineData(15, 11, 2008)]
+        [InlineData(22, 8, 2000)]
+        public void TestAge(int years, int months, int days)
+        {
+            Assert.Equal($"Ваш возоаст: {DateTime.Now.Year - years} лет {DateTime.Now.Month - months} месяцев и {DateTime.Now.Day - days} дня", YourAge.YourAgeNow(days, months, years));
+        }*/
+    }
+}
---
+++ CourseApp/YourAge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseApp
+{
+    public class YourAge
+    {
+        public static DateTime DateСomparison(DateTime dat1, DateTime dat2)
+        {
+            if (dat1.Ticks < dat2.Ticks)
+            {
+                var diff = new DateTime(dat2.Ticks - dat1.Ticks);
+                return diff;
+            }
+
+            throw new ArgumentOutOfRangeException("Вас не существует");
+        }
+
+        public static string YourAgeNow()
+        {
+            Console.WriteLine("Введите ваш год рождения:");
+            int years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц вашего рождения:");
+            int months = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день вашего рождения:");
+            int days = Convert.ToInt32(Console.ReadLine());
+            DateTime result = DateСomparison(new DateTime(years, months, days), DateTime.Now);
+            return $"Ваш возоаст: {result.Year - 1} лет {result.Month - 1} месяцев и {result.Day - 1} дня";
+        }
+
+        public static string YourAgeNow(int y, int m, int d)
+        {
+            DateTime result = DateСomparison(new DateTime(y, m, d), DateTime.Now);
+            return $"Вам {result.Year - 1} лет, {result.Month - 1} месяцев и {result.Day - 1} дня";
+        }
+
+        public static string YouAgeNow(DateTime dat)
+        {
+            DateTime result = DateСomparison(dat, DateTime.Now);
+            return $"Ваш возоаст: {result.Year - 1} лет {result.Month - 1} месяцев и {result.Day - 1} дня";
+        }
+    }
+}
